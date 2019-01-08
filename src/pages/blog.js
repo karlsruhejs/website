ba59828d1e976e5213fa8633d914354d2a67beb1
Emma Bostian <Emma.Bostian@logmein.com>
2019-01-08 12:58:09 +0100

    Adding menu sidebar

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,17 +2,21 @@ import React from 'react'
 import Layout from '../components/layout/layout'
 import { Link, graphql } from 'gatsby'
 
+import BlogMenu from '../components/blogMenu/blogMenu'
+
 import './blog.css'
 
 const BlogPage = ({ data }) => (
   <Layout>
     <h1 className="title">Blog</h1>
+    <BlogMenu />
     <section className="blog__section">
       {data.allMarkdownRemark.edges.map(post => (
-        <Link to={post.node.frontmatter.path}>
-          {post.node.frontmatter.title}
-          {post.node.frontmatter.description}
-        </Link>
+        <div key={post.node.frontmatter.date}>
+          <h1>{post.node.frontmatter.title}</h1>
+          <p>{post.node.frontmatter.description}</p>
+          <Link to={post.node.frontmatter.path}>Keep reading</Link>
+        </div>
       ))}
     </section>
   </Layout>
@@ -20,8 +24,8 @@ const BlogPage = ({ data }) => (
 
 export default BlogPage
 
-export const pageQuery = graphql`
-  query BlogIndexQuery {
+export const query = graphql`
+  query {
     allMarkdownRemark {
       edges {
         node {
